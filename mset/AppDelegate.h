cfb487073e4f06fe8c967afbb7e416e2b06957bd
Jonathan Slater <jon.slater@ymail.com>
2015-04-16 10:48:21 +0100

    initial commit

---
+++ mset/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mset
+//
+//  Created by Jonathan Slater on 16/04/2015.
+//  Copyright (c) 2015 Jonathan Slater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mset/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  mset
+//
+//  Created by Jonathan Slater on 16/04/2015.
+//  Copyright (c) 2015 Jonathan Slater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
