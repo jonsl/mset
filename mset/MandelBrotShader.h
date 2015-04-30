d773d6d4c6ad166c4864a63919803ce37a641a0f
Jonathan Slater <jonsl.dev@gmail.com>
2015-04-30 09:02:31 +0100

    missing files

---
+++ mset/MandelBrotShader.h
@@ -0,0 +1,7 @@
+//
+// Created by Jonathan Slater on 29/04/15.
+// Copyright (c) 2015 Jonathan Slater. All rights reserved.
+//
+
+@interface MandelBrotShader : NSObject<Shading>
+@end
---
+++ mset/MandelBrotShader.m
@@ -0,0 +1,19 @@
+//
+// Created by Jonathan Slater on 29/04/15.
+// Copyright (c) 2015 Jonathan Slater. All rights reserved.
+//
+
+#import "Mset.h"
+
+
+@implementation MandelBrotShader
+
+-(NSString*)vertexShader:(Texture*)texture {
+    return @"";
+}
+
+-(NSString*)fragmentShader:(Texture*)texture {
+    return @"";
+}
+
+@end
